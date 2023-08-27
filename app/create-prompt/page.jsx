af4408eaa3cc40a0e22ab4b5690a4b65ac05aac9
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-08-27 17:05:38 -0400

    finished create prompt functionality

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,13 @@
 "use client";
 import Form from "@components/Form";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -14,13 +19,13 @@ const CreatePrompt = () => {
     setSubmitting(true);
 
     try {
-      const response = await fetch("api/prompt/new", {
+      const response = await fetch("/api/prompt/new", {
         method: "POST",
-        body: {
+        body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
           tag: post.tag,
-        },
+        }),
       });
       if (response.ok) {
         router.push("/");
