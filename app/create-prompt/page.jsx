d59d00f7d7d68c2ef14b6f8c8445f3a71eec1313
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-08-27 16:06:53 -0400

    implemented form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import Form from "@components/Form";
+import React, { useState } from "react";
 
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
