5ab74cad5619c522615241d79a3f9d0091e86a02
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-08-28 14:30:07 -0400

    feed card completed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,30 @@
+"use client";
+import Image from "next/image";
 import React from "react";
+import { useState } from "react";
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            className="rounded-full object-contain"
+            width={40}
+            height={40}
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font">{post.creator.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PromptCard;
