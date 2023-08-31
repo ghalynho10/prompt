9029fd8908e314ba6c282024cdc6f60d1cafbbbe
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-08-31 13:13:28 -0400

    profile page.jsx completed

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import React, { useState } from "react";
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [allPosts, setAllPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/posts`);
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = async () => {};
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={allPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default page;
+export default MyProfile;
