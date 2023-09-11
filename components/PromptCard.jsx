659280dcb90cbd9fe5820ae49d2395d50139b800
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-09-11 13:04:05 -0400

    profile page completed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,15 @@
 "use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -34,8 +40,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           <Image
             src={
               copied === post.prompt
-                ? "assets/icons/tick.svg"
-                : "assets/icons/copy.svg"
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
             }
             width={12}
             height={12}
@@ -50,6 +56,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t pt-3 border-gray-100">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
